267c3e0690e0c370562011fb876f6f067cc0fdac
Golokhvastov Dmitry <dargon89@mail.ru>
2019-11-05 16:03:34 +0700

    Убрал магические числа в константы

--- js/stat.js
+++ js/stat.js
@@ -5,10 +5,14 @@ var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
+var FONT_GAP = 20;
+var TEXT_FONT = "16px PT Mono";
 
 var COLUMN_WIDTH = 40;
 var COLUMN_DISTANCE = 50;
 var HISTOGRAM_HEIGHT = 150;
+var HISTOGRAM_X = 40;
+var HISTOGRAM_Y = 90;
 
 var renderCloud = function(ctx, color, x, y) {
   ctx.fillStyle = color;
@@ -17,7 +21,7 @@ var renderCloud = function(ctx, color, x, y) {
 
 var renderText = function(ctx, text, x, y) {
   ctx.fillStyle = "#000000";
-  ctx.font = "16px PT Mono";
+  ctx.font = TEXT_FONT;
   ctx.fillText(text, x, y);
 }
 
@@ -40,8 +44,8 @@ window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, "rgba(0, 0, 0, 0.7)", CLOUD_X + GAP, CLOUD_Y + GAP);
   renderCloud(ctx, "#ffffff", CLOUD_X, CLOUD_Y);
 
-  renderText(ctx, "Ура вы победили!", CLOUD_X + 20, CLOUD_Y + 30);
-  renderText(ctx, "Список результатов:", CLOUD_X + 20, CLOUD_Y + 50);
+  renderText(ctx, "Ура вы победили!", CLOUD_X + FONT_GAP, CLOUD_Y + GAP + FONT_GAP);
+  renderText(ctx, "Список результатов:", CLOUD_X + FONT_GAP, CLOUD_Y + GAP + FONT_GAP * 2);
 
   var maxTime = getMaxElement(times);
 
@@ -52,8 +56,8 @@ window.renderStatistics = function (ctx, names, times) {
 
     var columnHeight = Math.floor(HISTOGRAM_HEIGHT * (times[i] / maxTime));
 
-    var columnX = CLOUD_X + 40 + (COLUMN_WIDTH + COLUMN_DISTANCE) * i;
-    var columnY = CLOUD_Y + 90 + (HISTOGRAM_HEIGHT - columnHeight);
+    var columnX = CLOUD_X + HISTOGRAM_X + (COLUMN_WIDTH + COLUMN_DISTANCE) * i;
+    var columnY = CLOUD_Y + HISTOGRAM_Y + (HISTOGRAM_HEIGHT - columnHeight);
 
     if (names[i] === "Вы") {
       var columnColor = "rgba(255, 0, 0, 1)";
@@ -63,7 +67,7 @@ window.renderStatistics = function (ctx, names, times) {
 
     renderColumn(ctx, columnColor, columnX, columnY, columnHeight);
 
-    renderText(ctx, Math.floor(times[i]) + "", columnX, columnY - 10);
-    renderText(ctx, names[i], columnX, columnY + columnHeight + 20);
+    renderText(ctx, Math.floor(times[i]) + "", columnX, columnY - FONT_GAP / 2);
+    renderText(ctx, names[i], columnX, columnY + columnHeight + FONT_GAP);
   }
 }
