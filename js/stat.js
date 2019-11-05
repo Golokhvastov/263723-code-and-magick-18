c45aa39bdaf091ccd1402a520d5a5b5c58dcaa7e
Golokhvastov Dmitry <dargon89@mail.ru>
2019-11-05 14:12:10 +0700

    Нарисовал гистограмму времен игроков

--- js/stat.js
+++ js/stat.js
@@ -2,6 +2,13 @@
 
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+
+var COLUMN_WIDTH = 40;
+var COLUMN_DISTANCE = 50;
+var HISTOGRAM_HEIGHT = 150;
 
 var renderCloud = function(ctx, color, x, y) {
   ctx.fillStyle = color;
@@ -14,10 +21,49 @@ var renderText = function(ctx, text, x, y) {
   ctx.fillText(text, x, y);
 }
 
-window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, "rgba(0, 0, 0, 0.7)", 110, 20);
-  renderCloud(ctx, "#ffffff", 100, 10);
+var renderColumn = function(ctx, color, x, y, height) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, COLUMN_WIDTH, height);
+};
 
-  renderText(ctx, "Ура вы победили!", 120, 40);
-  renderText(ctx, "Список результатов:", 120, 60);
+var getMaxElement = function(arr) {
+  var maxElement = arr[0];
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+  return maxElement;
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, "rgba(0, 0, 0, 0.7)", CLOUD_X + GAP, CLOUD_Y + GAP);
+  renderCloud(ctx, "#ffffff", CLOUD_X, CLOUD_Y);
+
+  renderText(ctx, "Ура вы победили!", CLOUD_X + 20, CLOUD_Y + 30);
+  renderText(ctx, "Список результатов:", CLOUD_X + 20, CLOUD_Y + 50);
+
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < names.length; i++) {
+    if (i > 3) {
+      break;
+    }
+
+    var columnHeight = Math.floor(HISTOGRAM_HEIGHT * (times[i] / maxTime));
+
+    var columnX = CLOUD_X + 40 + (COLUMN_WIDTH + COLUMN_DISTANCE) * i;
+    var columnY = CLOUD_Y + 90 + (HISTOGRAM_HEIGHT - columnHeight);
+
+    if (names[i] === "Вы") {
+      var columnColor = "rgba(255, 0, 0, 1)";
+    } else {
+      var columnColor = "hsl(240, " + Math.floor(Math.random() * 100) + "%, 50%)";
+    }
+
+    renderColumn(ctx, columnColor, columnX, columnY, columnHeight);
+
+    renderText(ctx, Math.floor(times[i]) + "", columnX, columnY - 10);
+    renderText(ctx, names[i], columnX, columnY + columnHeight + 20);
+  }
 }
